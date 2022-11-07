620092aca681bcecd6930cfbad49d040318c53ac
Jacob Henriksen <Henriksen.Jacob@gmail.com>
2022-11-07 18:43:38 +0100

    Added hash and hmac functionality

--- Kryptering/Program.cs
+++ Kryptering/Program.cs
@@ -1,34 +1,43 @@
 ﻿
 using System.ComponentModel.DataAnnotations;
 using System.Security.Cryptography;
+using System.Text;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Write 1 for random numbers, 2 for caeser cypher");
-        char switch_on = Console.ReadKey().KeyChar;
-
-        switch (switch_on)
+        bool run = true;
+        while (run)
         {
-            case '1':
-                //Set timer
-                Console.WriteLine("Starting test for Random at {0:HH:mm:ss.fff}", DateTime.Now);
-                GetRandom();
-                //Random timer stop
-                Console.WriteLine("Ending test for Random at {0:HH:mm:ss.fff}", DateTime.Now);
-                Console.WriteLine("Starting test for RandomNumberGenerator at {0:HH:mm:ss.fff}", DateTime.Now);
-                GetRandomNumberGenerator();
-                //RandomNumberGenerator stop
-                Console.WriteLine("Ending test for RandomNumberGenerator {0:HH:mm:ss.fff}", DateTime.Now);
-                Console.WriteLine();
-                break;
-            case '2':
-                Encrypter.Prompt();
-                break;
-            default:
-                break;
-        }
+            Console.WriteLine("\nWrite 1 for random numbers, \n2 for caeser cypher, \n3 for different hashing types");
+            char switch_on = Console.ReadKey().KeyChar;
+
+            switch (switch_on)
+            {
+                case '1':
+                    //Set timer
+                    Console.WriteLine("Starting test for Random at {0:HH:mm:ss.fff}", DateTime.Now);
+                    GetRandom();
+                    //Random timer stop
+                    Console.WriteLine("Ending test for Random at {0:HH:mm:ss.fff}", DateTime.Now);
+                    Console.WriteLine("Starting test for RandomNumberGenerator at {0:HH:mm:ss.fff}", DateTime.Now);
+                    GetRandomNumberGenerator();
+                    //RandomNumberGenerator stop
+                    Console.WriteLine("Ending test for RandomNumberGenerator {0:HH:mm:ss.fff}", DateTime.Now);
+                    Console.WriteLine();
+                    break;
+                case '2':
+                    Encrypter.Prompt();
+                    break;
+                case '3':
+                    HashAndHmac.WriteHashingResults();
+                    break;
+                default:
+                    run = false;
+                    break;
+            }
+        } 
     }
     public static void GetRandom()
     {
@@ -57,7 +66,7 @@ class Program
     }
 }
 
-//Caeser cipher encrypter..
+//Caeser cipher encrypter.
 class Encrypter
 {
     public static char C_Cip(char ch, int key)
@@ -84,55 +93,141 @@ class Encrypter
         return C_Encip(input, 26 - key);
     }
     
-    //Make better Prompt
     public static void Prompt()
     {
-        Console.WriteLine("Type a string to encrypt:");
+        Console.WriteLine("\nType what you want to encrypt: ");
         string UserString = Console.ReadLine();
 
-        Console.WriteLine("\n");
-
-        Console.Write("Enter your Key");
+        Console.Write("\nProvide a key");
         int key = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("\n");
 
+        Console.WriteLine("\nEncrypted Data with key: " + key);
+        string encryptedString = C_Encip(UserString, key);
+        Console.WriteLine(encryptedString);
 
-        Console.WriteLine("Encrypted Data");
-
-        string cipherText = C_Encip(UserString, key);
-        Console.WriteLine(cipherText);
-        Console.Write("\n");
-
-        Console.WriteLine("Decrypted Data:");
-
-        string t = C_Decip(cipherText, key);
-        Console.WriteLine(t);
-        Console.Write("\n");
-
-        Console.ReadKey();
+        Console.WriteLine("\nDecrypted Data:");
+        string decryptedString = C_Decip(encryptedString, key);
+        Console.WriteLine(decryptedString + "\n");
     }
 }
 
-class Hashing
+public class HashAndHmac
 {
-    public static void GetHash()
+    public static void WriteHashingResults()
     {
-        MD5 md5 = MD5.Create();
-        var rnd = RandomNumberGenerator.Create();
-        byte[] input = new byte[20];
-        byte[] hashValue;
+        int keySize;
+        byte[]? computedKey = null;
+        
+        Console.WriteLine("\nWrite text to hash: ");
+        string textToHash = Console.ReadLine();
+        Console.WriteLine("\nDo you want to use regular hash(1) or hmac(2)?. Type '1' or '2' \n");
+        int choice = Convert.ToInt32(Console.ReadLine());
 
-        rnd.GetBytes(input);
-       
-        hashValue = md5.ComputeHash(input);
+        //Sets the key if the user choses hmac, if not it will be null
+        if (choice == 2)
+        {
+            Console.WriteLine("\n Provide a key number: \n");
+            keySize = Convert.ToInt32(Console.ReadLine());
+            computedKey = GenerateKey(keySize);
+        }
+
+        //Create the hash with timer set so we can se how fast it resolves 
+        DateTime md5_t = DateTime.Now;
+        var md5 = Md5(Encoding.UTF8.GetBytes(textToHash), computedKey);
+        DateTime md5_s = DateTime.Now;
+        TimeSpan md5_value = md5_s.Subtract(md5_t);
+
+        DateTime sha1_t = DateTime.Now;
+        var sha1 = Sha1(Encoding.UTF8.GetBytes(textToHash), computedKey);
+        DateTime sha1_s = DateTime.Now;
+        TimeSpan sha1_value = sha1_s.Subtract(sha1_t);
+
+        DateTime sha256_t = DateTime.Now;
+        var sha256 = Sha256(Encoding.UTF8.GetBytes(textToHash), computedKey);
+        DateTime sha256_s = DateTime.Now;
+        TimeSpan sha256_value = sha256_s.Subtract(sha256_t);
+
+        DateTime sha512_t = DateTime.Now;
+        var sha512 = Sha512(Encoding.UTF8.GetBytes(textToHash), computedKey);
+        DateTime sha512_s = DateTime.Now;
+        TimeSpan sha512_value = sha512_s.Subtract(sha512_t);
+
+        //Write results to console
+        Console.WriteLine("Md5: " + Convert.ToBase64String(md5));
+        Console.WriteLine("MD5 Resolved Timespan: " + md5_value + "\n");
+
+        Console.WriteLine("Sha1: " + Convert.ToBase64String(sha1));
+        Console.WriteLine("SHA1 Resolved Timespan: " + sha1_value + "\n");
+
+        Console.WriteLine("Sha256: " + Convert.ToBase64String(sha256));
+        Console.WriteLine("SHA256 Resolved Timespan: " + sha256_value + "\n");
+
+        Console.WriteLine("Sha512: " + Convert.ToBase64String(sha512));
+        Console.WriteLine("SHA512 Resolved Timespan: " + sha512_value + "\n");
+
     }
-    public static void FilestreamHash()
+
+    public static byte[] GenerateKey(int KeySize)
     {
-        MD5 md5 = MD5.Create();
-        byte[] hashValue;
+        using var randomNumberGenerator = RandomNumberGenerator.Create();
+        var randomNumber = new byte[KeySize];
+        randomNumberGenerator.GetBytes(randomNumber);
 
-        FileStream fileStream = new FileStream("C:", FileMode.Open);
-        hashValue= md5.ComputeHash(fileStream);
+        return randomNumber;
     }
-
+    public static byte[] Sha256(byte[] textToHash, byte[]? key)
+    {
+        if(key != null)
+        {
+            using var hmac = new HMACSHA256(key);
+            return hmac.ComputeHash(textToHash);
+        }
+        else
+        {
+            using var sha256 = SHA256.Create();
+            return sha256.ComputeHash(textToHash);
+        }
+    }
+    public static byte[] Sha1(byte[] textToHash, byte[]? key)
+    {
+        if (key != null)
+        {
+            using var hmac = new HMACSHA1(key);
+            return hmac.ComputeHash(textToHash);
+        }
+        else
+        {
+            using var sha1 = SHA1.Create();
+            return sha1.ComputeHash(textToHash);
+        }
+        
+    }
+    public static byte[] Sha512(byte[] textToHash, byte[]? key)
+    {
+        if (key != null)
+        {
+            using var hmac = new HMACSHA512(key);
+            return hmac.ComputeHash(textToHash);
+        }
+        else
+        {
+            using var sha512 = SHA512.Create();
+            return sha512.ComputeHash(textToHash);
+        }
+        
+    }
+    public static byte[] Md5(byte[] textToHash, byte[]? key)
+    {
+        if (key != null)
+        {
+            using var hmac = new HMACMD5(key);
+            return hmac.ComputeHash(textToHash);
+        }
+        else
+        {
+            using var md5 = MD5.Create();
+            return md5.ComputeHash(textToHash);
+        }
+        
+    }
 }
